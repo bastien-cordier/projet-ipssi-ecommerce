54e7c3ed1cb2285236817f7a478831e8c48adf3d
Bastien Cordier <bastien.cordier78@gmail.com>
2021-07-20 21:19:13 +0200

    Modification Laravel vers Symfony

--- db.php
+++
@@ -1,5 +0,0 @@
-<!-- Voila le chemin de connexion vers la base de données PhpMyAdmin, user:root & mdp:root -->
-<?php
-	$bdd = mysqli_connect("localhost","root","root","db_prideon");
-	$bdd->set_charset("utf8");
-?>
