47ca739a01c23a988c35ac3cb42ef1a3a78050bc
Bastien Cordier <bastien.cordier78@gmail.com>
2021-08-16 20:36:44 +0200

    Update fonctionnelle

---
+++ migrations/Version20210816183416.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20210816183416 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE product CHANGE size size VARCHAR(255) NOT NULL, CHANGE price price VARCHAR(255) NOT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE product CHANGE price price INT NOT NULL, CHANGE size size INT NOT NULL');
+    }
+}
